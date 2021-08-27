25a52f97346decfc280b078e4e35171030512b69
Roshan Kumar Dalai <roshankd06@gmail.com>
2021-08-27 16:56:20 +0530

    Add files via upload

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import './Button.css';
+ 
+
+const Button = props => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
